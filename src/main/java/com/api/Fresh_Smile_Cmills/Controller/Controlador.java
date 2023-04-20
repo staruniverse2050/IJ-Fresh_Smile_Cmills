2901f6f1c9e6f1e7c0a4df9826d679cd704e0a00
Maite Martinez <salomeydavid507@gmail.com>
2023-04-20 16:10:10 -0500

    Modificación de archivos

--- src/main/java/com/api/Fresh_Smile_Cmills/Controller/Controlador.java
+++ src/main/java/com/api/Fresh_Smile_Cmills/Controller/Controlador.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("CRUDRepo")
+@RequestMapping("FreshSmile")
 public class Controlador {
     @Autowired
     private PacienteServiceIMPL impl;
